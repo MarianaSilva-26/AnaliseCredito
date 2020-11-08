ec2e753fe7539f20974071a053f4511be8fc4d89
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-11-08 14:16:27 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace AnaliseCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int  parcelas;
+        decimal valor; 
+        decimal valorParcela; 
+        decimal valorMaximoParcela; 
+        decimal rendaMensal;
+        bool  valorValido , parcelasValido , rendaMensalValido;
+
+        Console.Write("Digite o Valor Desejado: ");
+        valorValido = Decimal.TryParse(Console.ReadLine(), out valor);
+            
+        if (!valorValido)
+        {
+        Console.WriteLine();
+        Console.WriteLine("Você digitou um valor invalido");
+        Console.WriteLine("Pressione ENTER para sair, e execute novamente");
+        Console.ReadKey();
+        Environment.Exit(-1);
+        }
+
+        Console.Write("Digite a Quantidade de Parcelas Desejada: ");
+        parcelasValido = int.TryParse(Console.ReadLine(), out parcelas);
+
+        if (!parcelasValido)
+        {
+        Console.WriteLine();
+        Console.WriteLine("Você digitou um valor invalido");
+        Console.WriteLine("Pressione ENTER para sair, e execute novamente");
+        Console.ReadKey();
+        Environment.Exit(-1);
+        }
+
+        Console.Write("Digite a Sua Renda Mensal: ");
+        rendaMensalValido = Decimal.TryParse(Console.ReadLine(), out rendaMensal);
+        Console.WriteLine();
+
+        if (!rendaMensalValido)
+        {
+        Console.WriteLine("Você digitou um valor invalido");
+        Console.WriteLine("Pressione ENTER para sair, e execute novamente");
+        Console.ReadKey();
+        Environment.Exit(-1);
+        }
+
+        valorParcela = valor / parcelas;
+        valorMaximoParcela = rendaMensal * 0.3m;
+
+        Console.WriteLine($"Sua Quantidade de Parcelas: {valorParcela:C}");
+        Console.WriteLine($"Parcela Maxima: {valorMaximoParcela:C}");
+
+        if (valorParcela <= valorMaximoParcela)
+        {
+        Console.WriteLine();
+        Console.WriteLine("Seu credito foi aprovado!");
+        }
+        else
+        {
+        Console.WriteLine();
+        Console.WriteLine("Seu credito não foi aprovado!");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para sair!");
+        Console.ReadKey();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AnaliseCredito
-Um cliente quer solicitar um empréstimo. 
+Um cliente quer solicitar um empréstimo.
+Receba o valor solicitado, a quantidade de parcelas desejada e a renda mensal comprovada. Só autorize empréstimos cuja parcela não ultrapasse 30% da renda (desconsidere os juros). 
